44cf2bca0b7c5380f1617edc2dda9653e1d3598a
M Irfan <dev.mirfan@gmail.com>
2024-06-02 01:57:26 +0700

    Ads - try integrate with google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-provider";
 import { Suspense } from "react";
+import Head from "next/head";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -22,6 +23,14 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4555201872253805"
+          crossOrigin="anonymous"
+        ></script>
+        <meta name="google-adsense-account" content="ca-pub-4555201872253805" />
+      </Head>
       <body className={poppins.className}>
         <AuthProvider>
           <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
